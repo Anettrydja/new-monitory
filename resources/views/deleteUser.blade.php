06e907816e47a606fc526f9b80baf7c239afed3b
Liisa Mikola <70939470+liismik@users.noreply.github.com>
2021-06-17 16:18:29 +0300

    delete user blade

--- resources/views/deleteUser.blade.php
+++ resources/views/deleteUser.blade.php
@@ -4,7 +4,7 @@
     <br>
     <div id="logoimage">
         <div id="logoimage">
-            <image src="<?php echo asset ('css/logotest.png')?>" alt="logo" height="100px" width="320px">
+        <image src="<?php echo asset ('css/logotest.png')?>" alt="logo" height="100px" width="320px">
         </div>
     </div>
     <br>
@@ -19,32 +19,36 @@
             </div>
             <div id="bottom">
                 <div id="profile">
-                    <img id="usericon" src="<?php echo asset('images/usericon.png')?>"></img>
+                <img id="usericon" src="<?php echo asset('images/usericon.png')?>"></img>
                     <span id="name">Test Testerino</span>
                 </div>
-                <div id="profilebutton">
-                    <button><a href="">Profiil/konto</a></button>
-                </div>
+                <button id="userpagebutton"><a href="">Profiil/konto</a></button>
                 <button id="logout"><a href=""a>Logi välja</a>
             </div>
         </div>
         <div id="container_two">
-            <table border = "1">
-                <tr>
-                <td>Eesnimi</td>
-                <td>Perekonnanimi</td>
-                <td>Email</td>
-                </tr>
-                @foreach ($user as $user)
-                <tr>
-                <td>{{ $user->firstname }}</td>
-                <td>{{ $user->lastname }}</td>
-                <td>{{ $user->username }}</td>
-                <td><button><a href = 'delete/{{ $user->id }}'>Kustuta</a></button></td>
-                </tr>
-                @endforeach
-                </table>
-                <button><a href="/users">Mine tagasi</a></button>
+            <div id="content">
+                <div id="titlecontainer">
+                    <h1 id="title">Kustuta kasutaja</h1>
+                </div>
+                    <table id="deleteusertable">
+                        <tr>
+                        <td id="firstnametd">Eesnimi</td>
+                        <td id="lastnametd">Perekonnanimi</td>
+                        <td id="emailtd">Email</td>
+                        </tr>
+                        @foreach ($user as $user)
+                        <tr>
+                        <td id="firstnameinfotd">{{ $user->firstname }}</td>
+                        <td id="lastnameinfotd">{{ $user->lastname }}</td>
+                        <td id="emailinfotd">{{ $user->username }}</td>
+                        <td id="userdeletebuttontd"><button id="userdeletebutton"><a href = 'delete/{{ $user->id }}'>Kustuta</a></button></td>
+                        </tr>
+                        @endforeach
+                    </table>
+                    <button id="gobackbutton"><a href="/users">Mine tagasi</a></button>
+                </div>
+            </div>
         </div>
     </div>
 </body>
